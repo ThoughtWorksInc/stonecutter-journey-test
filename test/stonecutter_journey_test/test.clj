3b0c7054b0b81e3cdc118a6aa90e830062c881bb
Christoph Wong <chwong@thoughtworks.com>
2015-07-06 15:49:22 +0100

    RS + CW |16| test and screenshot sign-in-form of stonecutter-client

--- test/stonecutter_journey_test/test.clj
+++ test/stonecutter_journey_test/test.clj
@@ -1,9 +1,39 @@
 (ns stonecutter-journey-test.test
   (:require [midje.sweet :refer :all]
+            [clojure.java.io :as io]
             [clj-webdriver.taxi :as wd]
             [clj-webdriver.core :as wc]))
 
-(fact "can go to client page"
-      (wd/set-driver! {:browser :firefox})
-      (wd/to "https://stonecutter-client.herokuapp.com")
-      (wd/current-url) => "https://stonecutter-client.herokuapp.com/")
+(defn wait-for-title [title]
+  (try
+    (wd/wait-until #(= (wd/title) title) 5000)
+    (catch Exception e
+      (prn (str ">>>>>>>>>>> Title never appeared:"))
+      (prn (str "Expected: " title))
+      (prn (str "Actual: " (wd/title)))
+      (throw e))))
+
+(def screenshot-directory "test/stonecutter_journey_test/screenshots")
+(def screenshot-number (atom 0))
+(defn screenshot [filename]
+  (prn (str "Screenshot: " filename))
+  (wd/take-screenshot :file (str screenshot-directory "/"
+                                 (format "%02d" (swap! screenshot-number + 1))
+                                 "_" filename ".png")))
+
+(defn clear-screenshots []
+  (doall (->> (io/file screenshot-directory)
+              file-seq
+              (filter #(re-matches #".*\.png$" (.getName %)))
+              (map io/delete-file))))
+
+(against-background
+  [(before :contents (do (wd/set-driver! {:browser :firefox})
+                         (clear-screenshots)))
+   (after :contents (do (wd/quit)))]
+
+  (fact "can go to client page"
+        (wd/to "https://stonecutter-client.herokuapp.com")
+        (wait-for-title "Home")
+        (screenshot "client_home_page")
+        (wd/current-url) => "https://stonecutter-client.herokuapp.com/login"))
